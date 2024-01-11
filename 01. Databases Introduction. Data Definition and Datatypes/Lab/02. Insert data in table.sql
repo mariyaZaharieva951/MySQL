916c6fe6814462b14d6090564bf2d8c14e47ed84
Mariya Zaharieva <mariqtrunkova@gmail.com>
2024-01-11 16:25:09 +0200

    Insert data

---
+++ 01. Databases Introduction. Data Definition and Datatypes/Lab/02. Insert data in table.sql
@@ -0,0 +1,43 @@
+SELECT * FROM employees;
+
+CREATE TABLE people (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+SELECT first_name, last_name FROM employees;
+
+CREATE TABLE categories(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+category_id INT NOT NULL 
+);
+
+INSERT INTO employees VALUES(10,"Pesho",NULL);
+
+INSERT INTO employees (first_name) VALUES("Gosho");
+
+SELECT * FROM employees;
+
+INSERT INTO employees (first_name, last_name) 
+VALUES("Mariya", "Zaharieva"),
+	  ("Deyan", "Zahariev"),
+      ("Beni", "Zaharieva");
+
+SELECT * FROM employees;
+
+
+
+
+
+
+
+
+
